59c4d029f4d555926aa411df9fee4fa8aaaac6c0
Vinciius Oliveira <vinicius.oliveira@4mti.com.br>
2022-08-09 13:25:19 -0300

    ajustes css

--- src/components/Tarefa.jsx
+++ src/components/Tarefa.jsx
@@ -7,20 +7,20 @@ export function Tarefa(props) {
     return (
       <Tr>
         <Td className={props.style}>{props.tarefa.descricao}</Td>
+        <Td className="validar">
+          <span
+            className="material-symbols-outlined"
+            onClick={() => props.validarTarefa(props.tarefa.id)}
+          >
+            done
+          </span>
+        </Td>
         <Td className="lixeira">
           <span
             className="material-symbols-outlined"
             onClick={() => props.excluirTarefa(props.tarefa.id)}
           >
             delete
-          </span>
-        </Td>
-        <Td>
-          <span
-            className="material-symbols-outlined"
-            onClick={() => props.validarTarefa(props.tarefa.id)}
-          >
-            done
           </span>
         </Td>
       </Tr>
